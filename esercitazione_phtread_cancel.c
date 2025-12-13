ab776dbc43456a084e658584798bf93e62472517
MatteoKalinowski <matteokalinowski@gmail.com>
2025-12-13 13:58:56 +0100

    esercizio

---
+++ esercitazione_phtread_cancel.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h> 
+
+void *codice_thread(void * arg){
+    while(-1){
+        printf("thread in esecuzione...\n");
+        sleep(1);
+    }
+    return NULL;
+}
+
+int main(){
+
+    int n;
+    pthread_t ptid;
+
+    pthread_create(&ptid, NULL, codice_thread, NULL);
+    scanf("%d", &n);
+    pthread_cancel(ptid);
+    sleep(5);
+}
+    
